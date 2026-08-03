8ae6a9abddc8ca7f979d552db27a310dea2cf3f5
Priyanka PS <pspriyanka2000official@gmail.com>
2026-08-03 22:51:26 +0530

    updated

--- priyanka_portfolio/portfolio/urls.py
+++ priyanka_portfolio/portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from  .views import *
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('download-resume', views.download_resume, name = "download_resume"),
-    path("contact/", views.contact, name="contact"),
+    path('', home, name = 'home'),
+    path('download-resume', download_resume, name = "download_resume"),
+    path("contact/", contact, name="contact"),
 ]
